2290861872370dcc36aba98ab0a128692f24a651
Adrià Llorens <adria_llo@hotmail.com>
2017-09-21 09:13:53 +0200

    TestRPC commands

--- truffle-config.js
+++ truffle-config.js
@@ -1,16 +1,21 @@
 module.exports = {
-  networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
-    },
-      rinkeby: {
-          host: "localhost", // Connect to geth on the specified
-          port: 8545,
-          from: "0xDbdaA17aa6a854fEE1E127e1917E0a98dad607d7", // default address to use for any transaction Truffle makes during migrations
-          network_id: 4,
-          gas: 4612388 // Gas limit used for deploys
-      }
-  }
+    networks: {
+        rpc: {
+            host: "localhost",
+            port: 8545
+        },
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*",
+            from: "0xAec3aE5d2BE00bfC91597d7A1b2c43818d84396A"
+        },
+        rinkeby: {
+            host: "localhost", // Connect to geth on the specified ip
+            port: 8545, // Geth port
+            from: "0xDbdaA17aa6a854fEE1E127e1917E0a98dad607d7", // default address to use for any
+            network_id: 4, // Network ID
+            gas: 4612388 // Gas limit used for deploys
+        }
+    }
 };
