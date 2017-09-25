bcd1b776a9fe8f99345dda60fa2a80c09b529c80
Adrià Llorens <adria_llo@hotmail.com>
2017-09-25 09:59:03 +0200

    Exec + deploy coins interface

--- exec/adri.js
+++ exec/adri.js
@@ -4,33 +4,24 @@ var VictorCoin = artifacts.require("./VictorCoin.sol");
 
 module.exports = function(callback) {
     var platform;
-    var adri;
-    var victor;
 
     // Pre deployed contract
     AdriPlatform.deployed().then(function(ins1) {
         platform = ins1;
-        AdriCoin.deployed().then(function(ins2) {
-            adri = ins2;
-            VictorCoin.deployed().then(function(ins3) {
-                victor = ins3;
-
-                return platform.getCoinText.call();
-            }).then(function(tx) {
-                console.log(tx);
-                return platform.setCoinText(adri.address);
-            }).then(function(tx) {
-                return platform.getCoinText.call();
-            }).then(function(tx) {
-                console.log(tx);
-                return platform.setCoinText(victor.address);
-            }).then(function(tx) {
-                return platform.getCoinText.call();
-            }).then(function(tx) {
-                console.log(tx);
-                callback();
-            });
-
-        });
+        return platform.getCoinText.call();
+    }).then(function(tx) {
+        console.log(tx);
+        return platform.setCoinText(VictorCoin.address);
+    }).then(function(tx) {
+        return platform.getCoinText.call();
+    }).then(function(tx) {
+        console.log(tx);
+        return platform.setCoinText(AdriCoin.address);
+    }).then(function(tx) {
+        return platform.getCoinText.call();
+    }).then(function(tx) {
+        console.log(tx);
+        callback();
     });
+
 };
--- migrations/3_deploy_adri.js
+++ migrations/3_deploy_adri.js
@@ -1,17 +1,11 @@
 var AdriPlatform = artifacts.require("./AdriPlatform.sol");
-// var EasyCoinInterface = artifacts.require("./EasyCoinInterface.sol");
 var AdriCoin = artifacts.require("./AdriCoin.sol");
 var VictorCoin = artifacts.require("./VictorCoin.sol");
+var XaviCoin = artifacts.require("./XaviCoin.sol");
 
-module.exports = function(dd) {
-    var deployer = dd;
-
-    // deployer.deploy(EasyCoinInterface).then(function() {
-        return deployer.deploy(AdriPlatform).then(function() {
-            return deployer.deploy(AdriCoin).then(function() {
-                return deployer.deploy(VictorCoin);
-            })
-        })
-    // });
-
+module.exports = function(deployer) {
+    var contracts = [AdriPlatform, AdriCoin, VictorCoin, XaviCoin];
+    contracts.forEach(function(contract) {
+        deployer.deploy(contract);
+    });
 };
