ea51075ca0d38e1315400a8ca25271cbe1964ad9
Adria Llorens <adria_llo@hotmail.com>
2017-10-03 22:05:55 +0200

    Homework

--- exec/test.js
+++ exec/test.js
@@ -1,18 +1,34 @@
 var WitcoinPlatform = artifacts.require("./WitcoinPlatform.sol");
 var WitcoinSupply = artifacts.require("./WitcoinSupply.sol");
 var WitCoin = artifacts.require("./WitCoin.sol");
+var Utils = require("./util.js");
 
 module.exports = function(callback) {
-    var platform;
-    var coin;
 
-    // Pre deployed contract
-    WitcoinPlatform.deployed().then(function(instance) {
-        return instance.getWitcoinSupplyAddress.call();
-    }).then(function(tx) {
-        console.log(tx);
+    var instances = {
+        coin: WitCoin,
+        platform: WitcoinPlatform,
+        supply: WitcoinSupply
+    };
+
+    Utils.deployMultiple(instances).then(function (deployed) {
+        instances = Utils.parseDeployed(instances, deployed);
+        instances.coin.transfer("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84396A", 100000000).then(function(tx) {
+            console.log(tx.receipt.gasUsed);
+        });
     }).catch(function(e) {
         console.log(e);
     });
+
+
+    // Pre deployed contract
+    // WitCoin.deployed().then(function(instance) {
+    //     console.log(WitCoin.deployed());
+    //     WitCoin.deployed().transfer("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84396A", 100000000).then(function(tx) {
+    //         console.log(tx.receipt.gasUsed);
+    //     });
+    // }).catch(function(e) {
+    //     console.log(e);
+    // });
 
 };
