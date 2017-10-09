3c1c0cbc8755d1a4fae5acef907b638e8e0afcb4
Adrià Llorens <adria_llo@hotmail.com>
2017-10-09 14:13:41 +0200

    Deploy to main

--- migrations/1_deploy_crowdsale.js
+++ migrations/1_deploy_crowdsale.js
@@ -1,14 +1,11 @@
 var WitCoin = artifacts.require("./WitCoin.sol");
 var WitcoinCrowdsale = artifacts.require("./WitcoinCrowdsale.sol");
-var SampleContract = artifacts.require("./SampleContract.sol");
 
 module.exports = function(deployer) {
     return deployer.deploy(WitCoin).then(function(){
-        return deployer.deploy(SampleContract).then(function(){
-            return deployer.deploy(WitcoinCrowdsale, WitCoin.address, "0x04CF6551a4e0810C32DedBd76228b715f2598A33").then(function() {
-                return WitCoin.deployed().then(function(coin) {
-                    return coin.changeMinter(WitcoinCrowdsale.address);
-                });
+        return deployer.deploy(WitcoinCrowdsale, WitCoin.address, "0x04CF6551a4e0810C32DedBd76228b715f2598A33").then(function() {
+            return WitCoin.deployed().then(function(coin) {
+                return coin.changeMinter(WitcoinCrowdsale.address);
             });
         });
     });
