503a2e676a26d2722d2bc0ce2f04c5dcb105954f
Victor Muñoz <victormunoz@easyinnova.com>
2017-10-16 11:56:58 +0200

    Ico tests

--- test/ico-test.js
+++ test/ico-test.js
@@ -8,7 +8,11 @@ var promises = [];
 var address1 = "0xAec3aE5d2BE00bfC91597d7A1b2c43818d84396A";
 var address2 = "0xf1f42f995046E67b79DD5eBAfd224CE964740Da3";
 var address3 = "0x2052d46d53107b0384503be3a11935f0b5cd5342";
-var real_address = "0x04CF6551a4e0810C32DedBd76228b715f2598A33";
+var bounties = "0x057Afd5422524d5Ca20218d07048300832323360";
+var nirvana = "0x094d57AdaBa2278de6D1f3e2F975f14248C3775F";
+var team = "0x7eC9d37163F4F1D1fD7E92B79B73d910088Aa2e7";
+var club = "0xb2c032aF1336A1482eB2FE1815Ef301A2ea4fE0A";
+var receiver = "0x04CF6551a4e0810C32DedBd76228b715f2598A33";
 
 function EtherToWei(val) {
     return 1000000000000000000*val;
@@ -32,6 +36,8 @@ function AddDays(date, days) {
 
 contract('WitCoin', function(accounts) {
     var today = parseInt(new Date().getTime()/1000);
+    var initial_ether = WeiToEther(web3.eth.getBalance(address1).valueOf());
+    var initial_ether_receiver = WeiToEther(web3.eth.getBalance(receiver).valueOf());
 
     // Presale time
     it("set initial testing parameters", function() {
@@ -40,7 +46,8 @@ contract('WitCoin', function(accounts) {
             var startPresale = AddDays(today, -1);
             var startSale = AddDays(today, 1);
             var end = AddDays(today, 10);
-            return crowdsale.ChangeParameters(startSale, startPresale, end, WitcoinsToDecimals(1000000));
+            var endRefund = AddDays(today, 20);
+            return crowdsale.ChangeParameters(startSale, startPresale, end, endRefund, WitcoinsToDecimals(1000000));
         }).catch(function(e) {
             console.log(e);
             assert.equal(1, 2);
@@ -176,7 +183,24 @@ contract('WitCoin', function(accounts) {
         });
     });
 
-
+    it("check balances", function() {
+        return WitCoin.deployed().then(function (instance) {
+            coin = instance;
+            return CrowdSale.deployed().then(function (ins2) {
+                crowdsale = ins2;
+                var consumed_ether = initial_ether - WeiToEther(web3.eth.getBalance(address1).valueOf());
+                assert.equal(WeiToEther(web3.eth.getBalance("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84196A").valueOf()), 0);
+                assert.equal(parseInt(consumed_ether), 3);
+                assert.equal(WeiToEther(web3.eth.getBalance(address2).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(bounties).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(nirvana).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(team).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(club).valueOf()), 0);
+            });
+        });
+    });
 
 
     // Sale time
@@ -185,7 +209,8 @@ contract('WitCoin', function(accounts) {
             var startPresale = AddDays(today, -2);
             var startSale = AddDays(today, -1);
             var end = AddDays(today, 10);
-            return crowdsale.ChangeParameters(startSale, startPresale, end, WitcoinsToDecimals(1000000));
+            var endRefund = AddDays(today, 20);
+            return crowdsale.ChangeParameters(startSale, startPresale, end, endRefund, WitcoinsToDecimals(1000000));
         }).catch(function(e) {
             console.log(e);
             assert.equal(1, 2);
@@ -310,15 +335,36 @@ contract('WitCoin', function(accounts) {
         });
     });
 
+    it("check balances", function() {
+        return WitCoin.deployed().then(function (instance) {
+            coin = instance;
+            return CrowdSale.deployed().then(function (ins2) {
+                crowdsale = ins2;
+                assert.equal(WeiToEther(web3.eth.getBalance("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84196A").valueOf()), 0);
+                var consumed_ether = initial_ether - WeiToEther(web3.eth.getBalance(address1).valueOf());
+                assert.equal(WeiToEther(web3.eth.getBalance("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84196A").valueOf()), 0);
+                assert.equal(parseInt(consumed_ether), 14);
+                assert.equal(WeiToEther(web3.eth.getBalance(address2).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(bounties).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(nirvana).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(team).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(club).valueOf()), 0);
+            });
+        });
+    });
 
 
-    // Finalize crowdsale
+
+    // Finalize crowdsale goal not reached
     it("set initial testing parameters", function() {
         return CrowdSale.deployed().then(function(instance1) {
-            var startPresale = AddDays(today, -3);
-            var startSale = AddDays(today, -2);
-            var end = AddDays(today, -1);
-            return crowdsale.ChangeParameters(startSale, startPresale, end, WitcoinsToDecimals(1000000));
+            var startPresale = AddDays(today, -4);
+            var startSale = AddDays(today, -3);
+            var end = AddDays(today, -2);
+            var endRefund = AddDays(today, -1);
+            return crowdsale.ChangeParameters(startSale, startPresale, end, endRefund, WitcoinsToDecimals(1000000));
         }).catch(function(e) {
             console.log(e);
             assert.equal(1, 2);
@@ -373,10 +419,132 @@ contract('WitCoin', function(accounts) {
                 crowdsale = ins2;
                 return crowdsale.claimRefund();
             }).then(function(result) {
+                assert.equal(1, 2, "error in claim refund");
+            }).catch(function(e) {
+                assert.equal(1, 1, "error in claim refund");
+            });
+        });
+    });
+
+    it("check balances", function() {
+        return WitCoin.deployed().then(function (instance) {
+            coin = instance;
+            return CrowdSale.deployed().then(function (ins2) {
+                crowdsale = ins2;
+                assert.equal(WeiToEther(web3.eth.getBalance("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84196A").valueOf()), 0);
+                var consumed_ether = initial_ether - WeiToEther(web3.eth.getBalance(address1).valueOf());
+                assert.equal(WeiToEther(web3.eth.getBalance("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84196A").valueOf()), 0);
+                assert.equal(parseInt(consumed_ether), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(address2).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(bounties).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(nirvana).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(team).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(club).valueOf()), 0);
+            });
+        });
+    });
+
+
+
+    // Finalize crowdsale achieving goal
+    it("set final testing parameters", function() {
+        return CrowdSale.deployed().then(function(instance1) {
+            var startPresale = AddDays(today, -4);
+            var startSale = AddDays(today, -3);
+            var end = AddDays(today, -2);
+            var endRefund = AddDays(today, -1);
+            return crowdsale.ChangeParameters(startSale, startPresale, end, endRefund, WitcoinsToDecimals(0));
+        }).catch(function(e) {
+            console.log(e);
+            assert.equal(1, 2);
+        });
+    });
+
+    it("check claim refunds", function() {
+        return WitCoin.deployed().then(function (instance) {
+            coin = instance;
+            return CrowdSale.deployed().then(function (ins2) {
+                crowdsale = ins2;
+                return crowdsale.claimRefund();
+            }).then(function(result) {
                 assert.equal(1, 1, "error in claim refund");
             }).catch(function(e) {
                 assert.equal(1, 2, "error in claim refund");
             });
         });
     });
+
+    it("check balances", function() {
+        return WitCoin.deployed().then(function (instance) {
+            coin = instance;
+            return CrowdSale.deployed().then(function (ins2) {
+                crowdsale = ins2;
+                assert.equal(WeiToEther(web3.eth.getBalance("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84196A").valueOf()), 0);
+                var consumed_ether = initial_ether - WeiToEther(web3.eth.getBalance(address1).valueOf());
+                assert.equal(WeiToEther(web3.eth.getBalance("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84196A").valueOf()), 0);
+                assert.equal(parseInt(consumed_ether), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(address2).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(bounties).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(nirvana).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(team).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(club).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(receiver).valueOf()), initial_ether_receiver);
+            });
+        });
+    });
+
+    it("finalize refunding", function() {
+        return WitCoin.deployed().then(function (instance) {
+            coin = instance;
+            return CrowdSale.deployed().then(function (ins2) {
+                crowdsale = ins2;
+                return crowdsale.finalizeRefunding();
+            }).then(function(result) {
+                assert.equal(1, 1, "error in finalizing");
+            }).catch(function(e) {
+                assert.equal(1, 2, "error in finalizing");
+            });
+        });
+    });
+
+    it("check claim refunds", function() {
+        return WitCoin.deployed().then(function (instance) {
+            coin = instance;
+            return CrowdSale.deployed().then(function (ins2) {
+                crowdsale = ins2;
+                return crowdsale.claimRefund();
+            }).then(function(result) {
+                assert.equal(1, 2, "error in claim refund");
+            }).catch(function(e) {
+                assert.equal(1, 1, "error in claim refund");
+            });
+        });
+    });
+
+
+    it("check balances", function() {
+        return WitCoin.deployed().then(function (instance) {
+            coin = instance;
+            return CrowdSale.deployed().then(function (ins2) {
+                crowdsale = ins2;
+                assert.equal(WeiToEther(web3.eth.getBalance("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84196A").valueOf()), 0);
+                var consumed_ether = initial_ether - WeiToEther(web3.eth.getBalance(address1).valueOf());
+                assert.equal(WeiToEther(web3.eth.getBalance("0xAec3aE5d2BE00bfC91597d7A1b2c43818d84196A").valueOf()), 0);
+                assert.equal(parseInt(consumed_ether), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(address2).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(address3).valueOf()), 1000);
+                assert.equal(WeiToEther(web3.eth.getBalance(bounties).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(nirvana).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(team).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(club).valueOf()), 0);
+                assert.equal(WeiToEther(web3.eth.getBalance(receiver).valueOf()), initial_ether_receiver);
+            });
+        });
+    });
+
 });
