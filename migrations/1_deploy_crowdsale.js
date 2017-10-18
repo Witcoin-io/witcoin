f277c3b74b5d2521ab3a78bf4e4a4096a0338be3
Adrià Llorens <adria_llo@hotmail.com>
2017-10-18 13:02:17 +0200

    Pay bounties

--- migrations/1_deploy_crowdsale.js
+++ migrations/1_deploy_crowdsale.js
@@ -1,13 +1,11 @@
 var WitCoin = artifacts.require("./WitCoin.sol");
 var WitcoinCrowdsale = artifacts.require("./WitcoinCrowdsale.sol");
 
-var sender = "0xAec3aE5d2BE00bfC91597d7A1b2c43818d84396A";
-
 module.exports = function(deployer) {
     return deployer.deploy(WitCoin).then(function(){
         return deployer.deploy(WitcoinCrowdsale, WitCoin.address, "0x04CF6551a4e0810C32DedBd76228b715f2598A33").then(function() {
             return WitCoin.deployed().then(function(coin) {
-                return coin.changeMinter(sender);
+                return coin.changeMinter(WitcoinCrowdsale.address);
             });
         });
     });
